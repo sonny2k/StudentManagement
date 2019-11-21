f537edfa1437bf5368be7e9c3e77a40c1dfe470b
sonny2k <thanhdepzaipro@gmail.com>
2019-11-21 15:37:52 +0700

    finish

---
+++ stdmanagement/std-management/StudentManagement.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace std_management
+{
+    class StudentManagement
+    {
+        public PM14010[] GetStudents()
+        {
+            var db = new OOPCSEntities();
+            var Students = db.PM14010.ToArray();
+            return Students;
+        }
+
+        public void CreateStudent(string Code, string Name, bool Gender, string Hometown)
+        {
+            var Student = new PM14010();
+            Student.Code = Code;
+            Student.Name = Name;
+            Student.Hometown = Hometown;
+            Student.Gender = Gender;
+
+            var db = new OOPCSEntities();
+            db.PM14010.Add(Student);
+            db.SaveChanges();
+        }
+
+        public void UpdateStudent(int id, string Code, string Name,bool Gender, string Hometown)
+        {
+            var db = new OOPCSEntities();
+            var oldStudent = db.PM14010.Find(id);
+            oldStudent.Code = Code;
+            oldStudent.Name = Name;
+            oldStudent.Gender = Gender;
+            oldStudent.Hometown = Hometown;
+
+            db.Entry(oldStudent).State = System.Data.Entity.EntityState.Modified;
+            db.SaveChanges();
+        }
+
+        public void DeleteStudent(int id)
+        {
+            var db = new OOPCSEntities();
+            var std = db.PM14010.Find(id);
+            db.PM14010.Remove(std);
+            db.SaveChanges();
+        }
+
+        public PM14010 GetStudent(int id)
+        {
+            var db = new OOPCSEntities();
+            var std = db.PM14010.Find(id);
+            return std;
+        }
+    }
+}
---
+++ stdmanagement/std-management/StudentView.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace std_management
+{
+    class StudentView
+    {
+        public StudentView(PM14010 T)
+        {
+            this.id = T.id;
+            this.Code = T.Code;
+            this.Name = T.Name;
+            if (T.Gender == true)
+                this.Gender = "Male";
+            if (T.Gender == false)
+                this.Gender = "Female";
+            this.Hometown = T.Hometown;
+        }
+        public int id { get; set; }
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public string Hometown { get; set; }
+    }
+}
